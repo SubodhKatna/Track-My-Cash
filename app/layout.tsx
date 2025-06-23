cf9896d0f55e97c20cc26d97c58d0aba85cf635e
SubodhKatna <subodhkatna@gmail.com>
2025-06-23 11:37:27 +0530

    try adding speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,35 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import './globals.css';
-import { ClerkProvider } from '@clerk/nextjs';
-import RootProviders from '@/components/providers/RootProviders';
-import { Toaster } from '@/components/ui/sonner';
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import RootProviders from "@/components/providers/RootProviders";
+import { Toaster } from "@/components/ui/sonner";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Track my Cash',
-  description: 'Manage your expenses',
+  title: "Track my Cash",
+  description: "Manage your expenses",
   icons: {
-    icon: '/favicon.png',
+    icon: "/favicon.png",
   },
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <ClerkProvider>
-      <html lang="en" className="dark" style={{ colorScheme: 'dark' }}>
+      <html lang="en" className="dark" style={{ colorScheme: "dark" }}>
         <body className={`${inter.className} flex flex-col min-h-screen`}>
           <Toaster richColors position="bottom-right" />
           <RootProviders>
             <div className="flex-1">{children}</div>
           </RootProviders>
+          <SpeedInsights />
         </body>
       </html>
     </ClerkProvider>
